90db61bc9fb351e72e35c6625887f1cadafed09e
tac33 <collins.tyrese99@gmail.com>
2024-07-22 20:16:58 -0400

    Initial commit

---
+++ src/main/java/com/tac33/learningexample/LearningExampleClient.java
@@ -0,0 +1,10 @@
+package com.tac33.learningexample;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningExampleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/tac33/learningexample/LearningExampleDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tac33.learningexample;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LearningExampleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
